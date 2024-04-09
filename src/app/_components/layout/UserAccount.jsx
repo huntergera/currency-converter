7e7a896fe418327c1a8645239f09bbd6140f2912
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2024-04-09 18:17:32 +0300

    fixed icons

--- src/app/_components/layout/UserAccount.jsx
+++ src/app/_components/layout/UserAccount.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function UserAccount() {
   return (
-    <Link href="/account" className='flex ml-auto gap-x-4 text-black hover:text-gray transition-colors hover:*:fill-gray'>
+    <Link href="/account" className='flex items-center ml-auto gap-x-4 text-black hover:text-gray transition-colors hover:*:fill-gray'>
       <Image
         src="/images/icons/enter-icon.svg"
         alt="enter icon"
--- src/app/converter/_components/HistoryItem.jsx
+++ src/app/converter/_components/HistoryItem.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function HistoryItem() {
   return (
-    <div className="flex p-4 bg-white rounded gap-4 w-[47%] text-18">
+    <div className="flex items-center p-4 bg-white rounded gap-4 w-[47%] text-18">
       <span className="text-gray-100">25.11.2020</span>
       <span className="text-gray">1000 UAH</span>
       <Image
         src="/images/icons/icon-arrow.svg"
         alt="icon"
-        width={14}
-        height={10}
+        width={17}
+        height={12}
       />
       <span className="text-gray">36,65 USD</span>
     </div>

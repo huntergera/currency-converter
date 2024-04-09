9d5b31d3f4f054196220ebea71361baa2eff0d0b
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2024-04-09 19:01:55 +0300

    fixed input

--- src/app/_components/ui/InputDate.jsx
+++ src/app/_components/ui/InputDate.jsx
@@ -1,7 +1,7 @@
 export default function InputDate ({className}) {
   return (
-    <div className="relative">
-      <input type='date' id="dateInput" className={`appearance-none rounded outline-none py-4 px-2 text-center border-gray-100 border border-solid text-gray font-medium text-20  ${className}`}/>
+    <div className={`relative ${className}`}>
+      <input type='date' id="dateInput" className={`appearance-none rounded outline-none py-4 px-2 text-center border-gray-100 border border-solid text-gray font-medium text-20 w-full`}/>
       <label htmlFor="dateInput" className="absolute top-1/2 translate-y-[-50%] right-2 bg-calendar-icon bg-no-repeat bg-center bg-contain w-6 h-6 bg-white pointer-events-none"></label>
     </div>
   )
